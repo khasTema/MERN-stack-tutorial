310bdb7eacf957bf57e7f0d09caab4705c4a69c0
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-05-05 12:54:05 +0200

    lesson 5 | validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // mongo db package
+const bcrypt = require('bcrypt') // package used to encrypt the password fro safe storagin in DB
+const validator = require('validator') // package to validate email and password
 
 const Schema = mongoose.Schema
 
@@ -13,5 +15,41 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+// static sign up method
+userSchema.statics.signup = async function (email, password) {
+    // it has to be a regular func not arrow because we use this keyword
+
+
+    // validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Passwod is not strong enough')
+    }
+
+
+    //-------------------------------------------
+    const exists =  await this.findOne({email})
+
+    if (exists) {
+        throw Error("Email already in use")
+    }
+
+    const salt =  await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({
+        email,
+        password: hash
+    })
+
+    return user
+
+}
 
 module.exports = mongoose.model('User', userSchema)
