f02ed3044e0b793a07acd01d1175842bec64313f
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-05-10 18:16:49 +0200

    lesson 6 | Auth Routing on FE

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useAuthContext } from "../hooks/useAuthContext"
 
 const WorkoutForm = () => {
   const { dispatch } = useWorkoutsContext()
@@ -10,8 +11,15 @@ const WorkoutForm = () => {
   const [error, setError] = useState(null)
   const [emptyFields, setEmptyFields] = useState([])
 
+  const { user } = useAuthContext()
+
   const handleSubmit = async (e) => {
     e.preventDefault()
+
+    if (!user) {
+      setError('You must be loged in!')
+      return
+    }
 
     const workout = {title, load, reps}
     
@@ -19,7 +27,8 @@ const WorkoutForm = () => {
       method: 'POST',
       body: JSON.stringify(workout),
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${user.token}`
       }
     })
     const json = await response.json()
